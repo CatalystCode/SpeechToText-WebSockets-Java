3f25d65ccdb3f8880eb80f81f4f2a0627dcbd79f
Clemens Wolff <clewolff@microsoft.com>
2017-05-24 15:30:56 -0700

    Add second available output format

--- src/main/java/com/github/catalystcode/fortis/speechtotext/websocket/Format.java
+++ src/main/java/com/github/catalystcode/fortis/speechtotext/websocket/Format.java
@@ -1,7 +1,9 @@
 package com.github.catalystcode.fortis.speechtotext.websocket;
 
+@SuppressWarnings("unused")
 public enum Format {
     SIMPLE("simple"),
+    DETAILED("detailed"),
     ;
 
     public final String value;
