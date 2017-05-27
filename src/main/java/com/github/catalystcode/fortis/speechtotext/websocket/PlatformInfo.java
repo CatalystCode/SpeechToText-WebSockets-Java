d282e1a42a5a373faf70c853322ee3d82d7673d4
Clemens Wolff <clewolff@microsoft.com>
2017-05-26 18:18:32 -0700

    Avoid reflection in json serialization

--- src/main/java/com/github/catalystcode/fortis/speechtotext/websocket/PlatformInfo.java
+++ src/main/java/com/github/catalystcode/fortis/speechtotext/websocket/PlatformInfo.java
@@ -4,34 +4,41 @@ import org.json.JSONObject;
 
 import static java.lang.System.getProperty;
 
-@SuppressWarnings("unused")
-public class PlatformInfo {
-    public Context getContext() { return new Context(); }
-
-    public class Context {
-        public final System getSystem() { return new System(); }
-        public final Os getOs() { return new Os(); }
-        public final Device getDevice() { return new Device(); }
-
-        public class System {
-            public final String getVersion() { return getenv("STTWS_SYSTEM_VERSION", "0.0.1"); }
-        }
-
-        public class Os {
-            public final String getPlatform() { return getProperty("os.name").split(" ")[0]; }
-            public final String getName() { return getProperty("os.name"); }
-            public final String getVersion() { return getProperty("os.version"); }
-        }
-
-        public class Device {
-            public final String getManufacturer() { return getenv("SSTWS_DEVICE_MANUFACTURER", "SpeechToText-Websockets-Java"); }
-            public final String getModel() { return getenv("SSTWS_DEVICE_MODEL", "SpeechToText-Websockets-Java"); }
-            public final String getVersion() { return getenv("SSTWS_DEVICE_VERSION", "0.0.1"); }
-        }
+class PlatformInfo {
+    String toJson() {
+        JSONObject json = new JSONObject();
+        json.put("context", createContext());
+        return json.toString();
     }
 
-    String toJson() {
-        return new JSONObject(this).toString();
+    private JSONObject createContext() {
+        JSONObject json = new JSONObject();
+        json.put("system", createSystem());
+        json.put("os", createOs());
+        json.put("device", createDevice());
+        return json;
+    }
+
+    private JSONObject createSystem() {
+        JSONObject json = new JSONObject();
+        json.put("version", getenv("STTWS_SYSTEM_VERSION", "0.0.1"));
+        return json;
+    }
+
+    private JSONObject createOs() {
+        JSONObject json = new JSONObject();
+        json.put("platform", getProperty("os.name").split(" ")[0]);
+        json.put("name", getProperty("os.name"));
+        json.put("version", getProperty("os.version"));
+        return json;
+    }
+
+    private JSONObject createDevice() {
+        JSONObject json = new JSONObject();
+        json.put("manufacturer", getenv("SSTWS_DEVICE_MANUFACTURER", "SpeechToText-Websockets-Java"));
+        json.put("model", getenv("SSTWS_DEVICE_MODEL", "SpeechToText-Websockets-Java"));
+        json.put("version", getenv("SSTWS_DEVICE_VERSION", "0.0.1"));
+        return json;
     }
 
     private static String getenv(String key, String defaultValue) {
