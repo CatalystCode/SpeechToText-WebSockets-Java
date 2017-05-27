7bff55436f8c1fe580fd2d744c43ffe2c6bb370e
Clemens Wolff <clewolff@microsoft.com>
2017-05-26 18:23:07 -0700

    Extract string constants

---
+++ src/main/java/com/github/catalystcode/fortis/speechtotext/constants/SpeechServiceSpeechConfig.java
@@ -0,0 +1,17 @@
+package com.github.catalystcode.fortis.speechtotext.constants;
+
+public final class SpeechServiceSpeechConfig {
+    private SpeechServiceSpeechConfig() {}
+
+    public static final String CONTEXT = "context";
+    public static final String SYSTEM = "SYSTEM";
+    public static final String OS = "OS";
+    public static final String DEVICE = "DEVICE";
+    public static final String SYSTEM_VERSION = "version";
+    public static final String OS_PLATFORM = "platform";
+    public static final String OS_NAME = "name";
+    public static final String OS_VERSION = "version";
+    public static final String DEVICE_MANUFACTURER = "manufacturer";
+    public static final String DEVICE_MODEL = "model";
+    public static final String DEVICE_VERSION = "version";
+}
--- src/main/java/com/github/catalystcode/fortis/speechtotext/websocket/PlatformInfo.java
+++ src/main/java/com/github/catalystcode/fortis/speechtotext/websocket/PlatformInfo.java
@@ -2,42 +2,43 @@ package com.github.catalystcode.fortis.speechtotext.websocket;
 
 import org.json.JSONObject;
 
+import static com.github.catalystcode.fortis.speechtotext.constants.SpeechServiceSpeechConfig.*;
 import static java.lang.System.getProperty;
 
 class PlatformInfo {
     String toJson() {
         JSONObject json = new JSONObject();
-        json.put("context", createContext());
+        json.put(CONTEXT, createContext());
         return json.toString();
     }
 
     private JSONObject createContext() {
         JSONObject json = new JSONObject();
-        json.put("system", createSystem());
-        json.put("os", createOs());
-        json.put("device", createDevice());
+        json.put(SYSTEM, createSystem());
+        json.put(OS, createOs());
+        json.put(DEVICE, createDevice());
         return json;
     }
 
     private JSONObject createSystem() {
         JSONObject json = new JSONObject();
-        json.put("version", getenv("STTWS_SYSTEM_VERSION", "0.0.1"));
+        json.put(SYSTEM_VERSION, getenv("STTWS_SYSTEM_VERSION", "0.0.1"));
         return json;
     }
 
     private JSONObject createOs() {
         JSONObject json = new JSONObject();
-        json.put("platform", getProperty("os.name").split(" ")[0]);
-        json.put("name", getProperty("os.name"));
-        json.put("version", getProperty("os.version"));
+        json.put(OS_PLATFORM, getProperty("os.name").split(" ")[0]);
+        json.put(OS_NAME, getProperty("os.name"));
+        json.put(OS_VERSION, getProperty("os.version"));
         return json;
     }
 
     private JSONObject createDevice() {
         JSONObject json = new JSONObject();
-        json.put("manufacturer", getenv("SSTWS_DEVICE_MANUFACTURER", "SpeechToText-Websockets-Java"));
-        json.put("model", getenv("SSTWS_DEVICE_MODEL", "SpeechToText-Websockets-Java"));
-        json.put("version", getenv("SSTWS_DEVICE_VERSION", "0.0.1"));
+        json.put(DEVICE_MANUFACTURER, getenv("SSTWS_DEVICE_MANUFACTURER", "SpeechToText-Websockets-Java"));
+        json.put(DEVICE_MODEL, getenv("SSTWS_DEVICE_MODEL", "SpeechToText-Websockets-Java"));
+        json.put(DEVICE_VERSION, getenv("SSTWS_DEVICE_VERSION", "0.0.1"));
         return json;
     }
 
