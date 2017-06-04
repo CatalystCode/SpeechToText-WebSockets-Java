339dbc4e69f933016bdc16786e161a9fb607644d
Clemens Wolff <clewolff@microsoft.com>
2017-06-04 19:02:50 +0200

    Add resampling support

--- src/main/java/com/github/catalystcode/fortis/speechtotext/messages/BinaryMessageCreator.java
+++ src/main/java/com/github/catalystcode/fortis/speechtotext/messages/BinaryMessageCreator.java
@@ -10,6 +10,7 @@ import static com.github.catalystcode.fortis.speechtotext.constants.SpeechServic
 import static com.github.catalystcode.fortis.speechtotext.messages.HeaderCreator.addHeaders;
 import static com.github.catalystcode.fortis.speechtotext.utils.RiffHeader.RIFF_HEADER_LENGHT;
 import static com.github.catalystcode.fortis.speechtotext.utils.RiffHeader.putRiffHeader;
+import static java.lang.Math.round;
 import static java.nio.ByteBuffer.allocate;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -41,7 +42,7 @@ public class BinaryMessageCreator {
         int offset = isFirstMessage ? RIFF_HEADER_LENGHT : 0;
         int length = isFirstMessage ? count - RIFF_HEADER_LENGHT : count;
         if (isFirstMessage) putRiffHeader(buf, SAMPLE_RATE, NUM_CHANNELS);
-        putAudio(buf, wavBytes, offset, length, sampleRate, SAMPLE_RATE);
+        putAudio(buf, wavBytes, offset, length);
     }
 
     private void updateState() {
@@ -56,9 +57,15 @@ public class BinaryMessageCreator {
     }
 
     private ByteBuffer allocateBuffer(int numWavBytes, int numHeaderBytes) {
-        int bufSize = 2 + numHeaderBytes + numWavBytes;
+        int bufSize = 2 + numHeaderBytes;
         if (isFirstMessage) bufSize += RIFF_HEADER_LENGHT;
+        if (!needsResampling()) bufSize += numWavBytes;
+        else bufSize += computeResampledNumBytes(numWavBytes);
         return allocate(bufSize);
+    }
+
+    private int computeResampledNumBytes(int numWavBytes) {
+        return (int)round(numWavBytes / (sampleRate / (double)SAMPLE_RATE));
     }
 
     private void setSampleRate(byte[] wavBytes) {
@@ -71,16 +78,27 @@ public class BinaryMessageCreator {
     }
 
     private boolean needsResampling() {
-        boolean needs = sampleRate != SAMPLE_RATE;
-        if (needs) {
-            throw new RuntimeException("foo");
-        }
-        return needs;
+        return sampleRate != SAMPLE_RATE;
     }
 
-    private void putAudio(ByteBuffer buf, byte[] wavBytes, int offset, int length, int actualSampleRate, int desiredSampleRate) {
+    private void putAudio(ByteBuffer buf, byte[] wavBytes, int offset, int length) {
         if (!needsResampling()) {
             buf.put(wavBytes, offset, length);
+            return;
+        }
+
+        double sampleRatio = sampleRate / (double)SAMPLE_RATE;
+        int offsetBuffer = 0;
+        int resampledNumBytes = computeResampledNumBytes(length);
+        for (int offsetResult = 0; offsetResult < resampledNumBytes; offsetResult++) {
+            int nextOffsetBuffer = (int)round((offsetResult + 1) * sampleRatio);
+            int accum = 0;
+            int count = 0;
+            for (int i = offsetBuffer; i < nextOffsetBuffer && i < length; i++, count++) {
+                accum += wavBytes[i + offset];
+            }
+            buf.put((byte)(accum / count));
+            offsetBuffer = nextOffsetBuffer;
         }
     }
 }
