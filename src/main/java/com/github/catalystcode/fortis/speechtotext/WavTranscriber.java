182cb301d41427a7a8dd9221ad91aa7922f2d559
Clemens Wolff <clewolff@microsoft.com>
2017-06-05 23:49:07 +0200

    Remove unnecessary try/catch

--- src/main/java/com/github/catalystcode/fortis/speechtotext/WavTranscriber.java
+++ src/main/java/com/github/catalystcode/fortis/speechtotext/WavTranscriber.java
@@ -3,13 +3,10 @@ package com.github.catalystcode.fortis.speechtotext;
 import com.github.catalystcode.fortis.speechtotext.config.SpeechServiceConfig;
 import com.github.catalystcode.fortis.speechtotext.websocket.MessageSender;
 import com.github.catalystcode.fortis.speechtotext.websocket.SpeechServiceClient;
-import org.apache.log4j.Logger;
 
 import java.io.InputStream;
 
 class WavTranscriber extends Transcriber {
-    private static final Logger log = Logger.getLogger(WavTranscriber.class);
-
     WavTranscriber(SpeechServiceConfig config, SpeechServiceClient client) {
         super(config, client);
     }
@@ -21,12 +18,8 @@ class WavTranscriber extends Transcriber {
 
     private void sendAudioAsync(InputStream wavStream, MessageSender sender) {
         new Thread(() -> {
-            try {
-                sender.sendAudio(wavStream);
-                sender.sendAudioEnd();
-            } catch (Exception ex) {
-                log.error("Error sending audio", ex);
-            }
+            sender.sendAudio(wavStream);
+            sender.sendAudioEnd();
         }).run();
     }
 }
