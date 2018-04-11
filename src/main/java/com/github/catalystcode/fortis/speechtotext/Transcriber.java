df0c12f26105954ec0642a854cc026c65c142f1a
Philipp Zell <zell.p@ad.zdf-digital.com>
2018-04-11 15:58:13 +0200

    add support for audio per inputstream

--- src/main/java/com/github/catalystcode/fortis/speechtotext/Transcriber.java
+++ src/main/java/com/github/catalystcode/fortis/speechtotext/Transcriber.java
@@ -11,42 +11,51 @@ import java.io.InputStream;
 import java.util.function.Consumer;
 
 public abstract class Transcriber {
-    protected final SpeechServiceConfig config;
-    private final SpeechServiceClient client;
+	protected final SpeechServiceConfig config;
+	private final SpeechServiceClient client;
 
-    Transcriber(SpeechServiceConfig config, SpeechServiceClient client) {
-        this.config = config;
-        this.client = client;
-    }
+	Transcriber(SpeechServiceConfig config, SpeechServiceClient client) {
+		this.config = config;
+		this.client = client;
+	}
 
-    public void transcribe(InputStream audioStream, Consumer<String> onResult, Consumer<String> onHypothesis) throws Exception {
-        MessageReceiver receiver = new MessageReceiver(onResult, onHypothesis, client.getEndLatch());
-        try {
-            MessageSender sender = client.start(config, receiver);
-            receiver.setSender(sender);
-            sender.sendConfiguration();
-            sendAudio(audioStream, sender);
-            client.awaitEnd();
-        } finally {
-            client.stop();
-        }
-    }
+	public void transcribe(InputStream audioStream, Consumer<String> onResult, Consumer<String> onHypothesis)
+			throws Exception {
+		MessageReceiver receiver = new MessageReceiver(onResult, onHypothesis, client.getEndLatch());
+		try {
+			MessageSender sender = client.start(config, receiver);
+			receiver.setSender(sender);
+			sender.sendConfiguration();
+			sendAudio(audioStream, sender);
+			client.awaitEnd();
+		} finally {
+			client.stop();
+		}
+	}
 
-    protected abstract void sendAudio(InputStream audioStream, MessageSender sender) throws IOException;
+	protected abstract void sendAudio(InputStream audioStream, MessageSender sender) throws IOException;
 
-    public static Transcriber create(String audioPath, SpeechServiceConfig config) {
-        return create(audioPath, config, new NvSpeechServiceClient());
-    }
+	public static Transcriber create(String audioPath, SpeechServiceConfig config) {
+		return create(audioPath, config, new NvSpeechServiceClient());
+	}
 
-    private static Transcriber create(String audioPath, SpeechServiceConfig config, SpeechServiceClient client) {
-        if (audioPath.endsWith(".wav")) {
-            return new WavTranscriber(config, client);
-        }
+	public static Transcriber create(SpeechServiceConfig config) {
+		return create(config, new NvSpeechServiceClient());
+	}
 
-        if (audioPath.endsWith(".mp3")) {
-            return new Mp3Transcriber(config, client);
-        }
+	private static Transcriber create(String audioPath, SpeechServiceConfig config, SpeechServiceClient client) {
+		if (audioPath.endsWith(".wav")) {
+			return new WavTranscriber(config, client);
+		}
 
-        throw new IllegalArgumentException("Unsupported audio file type: " + audioPath);
-    }
+		if (audioPath.endsWith(".mp3")) {
+			return new Mp3Transcriber(config, client);
+		}
+
+		throw new IllegalArgumentException("Unsupported audio file type: " + audioPath);
+	}
+
+	private static Transcriber create(SpeechServiceConfig config, SpeechServiceClient client) {
+		return new WavTranscriber(config, client);
+	}
 }
